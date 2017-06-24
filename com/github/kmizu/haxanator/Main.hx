da62b18cb63a8057070f02f66a70c1800e3b9dad
Kota Mizushima <mizukota@gmail.com>
2017-06-25 01:26:50 +0900

    Basic Combinators are implemented

--- com/github/kmizu/haxanator/Main.hx
+++
@@ -1,9 +0,0 @@
-package com.github.kmizu.haxanator;
-
-class Main {
-  static public function main() {
-    var parser = new LiteralParser("100");
-    var result = parser.parse("10000");
-    trace(result);
-  }
-}
--- src/main/haxe/com/github/kmizu/haxanator/ParseResult.hx
+++ src/main/haxe/com/github/kmizu/haxanator/ParseResult.hx
@@ -2,5 +2,5 @@ package com.github.kmizu.haxanator;
 
 enum ParseResult<T> {
   Success<T>(value: T, next: String);
-  Failure(next: String);
+  Failure(message: String);
 }
